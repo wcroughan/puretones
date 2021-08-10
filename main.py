1a78e525545ee527c59c1d08259203aa4793f8ff
wcroughan <croughanb@gmail.com>
2021-08-10 09:45:43 -0700

    general logic kinda fleshed out. Need to implement midi and also tone bend calculation

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from rtmidi.midiutil import open_midiinput, open_midioutput
 from rtmidi.midiconstants import NOTE_ON, NOTE_OFF
-from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout
+from PyQt5.QtWidgets import QWidget, QLabel, QApplication, QVBoxLayout,  QHBoxLayout, QComboBox
 import sys
 import time
+import numpy as np
 
 
 class PureTones(QWidget):
@@ -12,14 +13,49 @@ class PureTones(QWidget):
         self.currentNotes = []
         self.NUM_CHANNELS = 16
         self.MAX_NOTES = self.NUM_CHANNELS
+        self.TONE_METHOD_OPTIONS = ["None", "keyroot", "chordroot_et", "chordroot_key"]
+        self.tm = None
+
+        self.bendTimeRes = 20  # ms
+        self.currentBends = []
 
         self.initUI()
         self.initMIDI()
 
     def initUI(self):
-        layout = QVBoxLayout()
-        layout.addWidget(QLabel("HI!"))
-        self.setLayout(layout)
+        self.mainLayout = QVBoxLayout()
+
+        ls = QHBoxLayout()
+        ls.addWidget(QLabel("Method:"))
+        cb = QComboBox()
+        cb.addItems(self.TONE_METHOD_OPTIONS)
+        cb.currentIndexChanged.connect(self.toneMethodChanged)
+        ls.addWidget(cb)
+        self.mainLayout.addLayout(ls)
+
+        self.tmWidget = QLabel("")
+        self.mainLayout.addWidget(self.tmWidget)
+
+        self.setLayout(self.mainLayout)
+
+    def toneMethodChanged(self, idx):
+        tmname = self.TONE_METHOD_OPTIONS[idx]
+
+        if tmname == "None":
+            self.tm = None
+            self.mainLayout.removeWidget(self.tmWidget)
+            self.tmWidget = QLabel("")
+            self.mainLayout.addWidget(self.tmWidget)
+            return
+        elif tmname == "keyroot":
+            self.tm = TMKeyRoot()
+        else:
+            print("UNIMPLEMENTED")
+            return
+
+        self.mainLayout.removeWidget(self.tmWidget)
+        self.tmWidget = self.tm.widget
+        self.mainLayout.addWidget(self.tmWidget)
 
     def initMIDI(self):
         self.midiout, self.outPortName = open_midioutput()
@@ -59,7 +95,10 @@ class PureTones(QWidget):
         m = [status, pitch, vel]
         self.midiout.send_message(m)
         self.currentNotes.append((pitch, ch))
+        self.currentBends.append(0)
         print("on: {}".format(m))
+
+        self.recalculateBends()
 
     def noteOff(self, ch, pitch):
         status = NOTE_OFF | ch
@@ -69,8 +108,52 @@ class PureTones(QWidget):
         for ni, n in enumerate(self.currentNotes):
             if n[0] == pitch:
                 self.currentNotes.pop(ni)
+                self.currentBends.pop(ni)
                 break
         print("off: {}".format(m))
+
+        self.recalculateBends()
+
+    def recalculateBends(self):
+        if self.tm is not None:
+            self.bendEndPoints = self.tm.calculateBends([n[0] for n in self.currentNotes])
+        else:
+            self.bendEndPoints = [0] * len(self.currentNotes)
+        self.bendFrames = np.zeros(
+            (len(self.currentNotes), np.ceil(self.bendTime / self.bendTimeRes)))
+        bendT = np.arange(np.shape(self.bendFrames)[1])
+        for ni in range(len(self.bendEndPoints)):
+            self.bendFrames[ni, :] = np.interp(
+                bendT, [0, bendT[-1]], [self.currentBends[ni], self.bendEndPoints[ni]])
+        self.bendi = 0
+        self.bendTimer.start(self.bendTimeRes)
+
+    def bendFunc(self):
+        for ni, n in enumerate(self.currentNotes):
+            ch = n[1]
+            b = self.bendFrames[ni, self.bendi]
+            self.pb(ch, b)
+            self.currentBends[ni] = b
+
+        self.bendi += 1
+        if self.bendi >= np.shape(self.bendFrames)[1]:
+            self.bendTimer.stop()
+
+    def pb(self, channel, val):
+        print(channel, val)
+
+
+class ToneMethod:
+    def __init__(self):
+        self.widget = QWidget()
+
+    def calculateBends(self, notes):
+        return [0] * len(notes)
+
+
+class TMKeyRoot(ToneMethod):
+    def __init__(self):
+        super().__init__()
 
 
 if __name__ == "__main__":
